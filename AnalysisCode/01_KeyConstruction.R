27692a411b0b1d76b0c67721ad3e3ec59b45b72f
milandolez <73084411+milandolez@users.noreply.github.com>
2022-09-26 15:12:11 -0700

    Update 01_KeyConstruction.R

--- AnalysisCode/01_KeyConstruction.R
+++ AnalysisCode/01_KeyConstruction.R
@@ -25,4 +25,4 @@ key <- data.frame(scalevalues, temperature=numeric(12))
 key$temperature <- seq(35.6, 26.4, length.out=12)
 
 #save key as RData file for intermediate data usage
-#save(key, file="key.RData")
+#save(key, file="new_key.RData")
