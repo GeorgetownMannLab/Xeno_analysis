c1b46dae1c3c64e939989cb7810c2f409e60c7e7
milandolez <73084411+milandolez@users.noreply.github.com>
2022-09-26 14:25:53 -0700

    Update 01_KeyConstruction.R

--- AnalysisCode/01_KeyConstruction.R
+++ AnalysisCode/01_KeyConstruction.R
@@ -2,19 +2,23 @@
 
 library(raster)
 
-ir <- raster("newSfb218-75.tif")
+ir <- raster("RawData/newSfb218-75.tif")
 
 image(ir)
 
 #manually select color scale increments
-colorscale <- locator(12)
-cs <- as.data.frame(colorscale)
+##NOT RUN##
+#example code for manual selection only
+#colorscale <- locator(12)
+#cs <- as.data.frame(colorscale)
 
-scalevalues <- extract(ir, cs)
+#scalevalues <- extract(ir, cs)
 
-key <- data.frame(scalevalues, temperature=numeric(12))
+#key <- data.frame(scalevalues, temperature=numeric(12))
+####
 
 #assign temperature to the scale values
 key$temperature <- seq(35.6, 26.4, length.out=12)
 
-save(key, file="key.RData")
+#save key as RData file for intermediate data usage
+#save(key, file="key.RData")
